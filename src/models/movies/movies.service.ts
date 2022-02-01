faf3cec8d04a88fcb6e0f9a34bd886fe522b6c67
MarcosPaul0 <marcosphip7@gmail.com>
2022-01-31 23:03:51 -0300

    Add protect routes in movie service

--- src/models/movies/movies.service.ts
+++ src/models/movies/movies.service.ts
@@ -1,4 +1,11 @@
-import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { RoleGuard } from 'src/auth/guards/role.guard';
 import { PrismaService } from '../../prisma/prisma.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
@@ -8,6 +15,8 @@ import { Movie } from './entities/movie.entity';
 export class MoviesService {
   constructor(private readonly prismaService: PrismaService) {}
 
+  @UseGuards(RoleGuard)
+  @UseGuards(JwtAuthGuard)
   async create({
     name,
     direction,
@@ -28,12 +37,16 @@ export class MoviesService {
     return newMovie;
   }
 
+  @UseGuards(RoleGuard)
+  @UseGuards(JwtAuthGuard)
   async findAll(): Promise<Movie[]> {
     const allMovies = await this.prismaService.movie.findMany();
 
     return allMovies;
   }
 
+  @UseGuards(RoleGuard)
+  @UseGuards(JwtAuthGuard)
   async findByName(name: string): Promise<Movie[]> {
     const movies = await this.prismaService.movie.findMany({
       where: {
@@ -41,9 +54,18 @@ export class MoviesService {
       },
     });
 
+    if (!movies) {
+      throw new NotFoundException({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'Movie not found',
+      });
+    }
+
     return movies;
   }
 
+  @UseGuards(RoleGuard)
+  @UseGuards(JwtAuthGuard)
   async update(
     id: string,
     { name, genre, direction, launched_at, budget }: UpdateMovieDto,
