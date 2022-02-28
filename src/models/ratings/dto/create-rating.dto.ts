b1f194db34215c4af9b33418f60dd045f2d03fd3
MarcosPaulo <marcosphip7@gmail.com>
2022-02-27 22:24:31 -0300

    feat: add validations in ratings dto

--- src/models/ratings/dto/create-rating.dto.ts
+++ src/models/ratings/dto/create-rating.dto.ts
@@ -1,5 +1,31 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class CreateRatingDto {
+  @IsNotEmpty({
+    message: 'Field score is required',
+  })
+  @IsNumber(
+    {
+      allowInfinity: false,
+      allowNaN: false,
+    },
+    {
+      message: 'Field score is invalid format',
+    },
+  )
   score: number;
+
+  @IsNotEmpty({
+    message: 'Field movie_id is required',
+  })
+  @IsString({
+    message: 'Field movie_id is invalid format',
+  })
   movie_id: string;
+
+  @IsOptional()
+  @IsString({
+    message: 'Field user_id is invalid format',
+  })
   user_id?: string;
 }
--- src/models/ratings/dto/update-rating.dto.ts
+++ src/models/ratings/dto/update-rating.dto.ts
@@ -1,3 +1,17 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
 export class UpdateRatingDto {
+  @IsNotEmpty({
+    message: 'Field score is required',
+  })
+  @IsNumber(
+    {
+      allowInfinity: false,
+      allowNaN: false,
+    },
+    {
+      message: 'Field score is invalid format',
+    },
+  )
   score: number;
 }
