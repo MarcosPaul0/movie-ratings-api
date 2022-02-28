b5b549a6c35d97a39d65b6bb46c046e65c0126f6
MarcosPaulo <marcosphip7@gmail.com>
2022-02-27 22:23:58 -0300

    feat: add validations in movies dto

--- src/models/movies/dto/create-movie.dto.ts
+++ src/models/movies/dto/create-movie.dto.ts
@@ -1,28 +1,49 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateMovieDto {
   @IsNotEmpty({
     message: 'Name is missing',
+  })
+  @IsString({
+    message: 'Field name is invalid format',
   })
   name: string;
 
   @IsNotEmpty({
     message: 'Genre is missing',
   })
+  @IsString({
+    message: 'Field genre is invalid format',
+  })
   genre: string;
 
   @IsNotEmpty({
     message: 'Direction is missing',
+  })
+  @IsString({
+    message: 'Field direction is invalid format',
   })
   direction: string;
 
   @IsNotEmpty({
     message: 'Budget is missing',
   })
+  @IsNumber(
+    {
+      allowInfinity: false,
+      allowNaN: false,
+    },
+    {
+      message: 'Field budget is invalid format',
+    },
+  )
   budget: number;
 
   @IsNotEmpty({
     message: 'Launched date is missing',
+  })
+  @IsDate({
+    message: 'Field launched_at is invalid format',
   })
   launched_at: Date;
 }
--- src/models/movies/dto/update-movie.dto.ts
+++ src/models/movies/dto/update-movie.dto.ts
@@ -1,7 +1,34 @@
+import { IsDate, IsNumber, IsString } from 'class-validator';
+
 export class UpdateMovieDto {
+  @IsString({
+    message: 'Field name is invalid format',
+  })
   name?: string;
+
+  @IsString({
+    message: 'Field genre is invalid format',
+  })
   genre?: string;
+
+  @IsString({
+    message: 'Field direction is invalid format',
+  })
   direction?: string;
-  launched_at?: Date;
+
+  @IsNumber(
+    {
+      allowInfinity: false,
+      allowNaN: false,
+    },
+    {
+      message: 'Field budget is invalid format',
+    },
+  )
   budget?: number;
+
+  @IsDate({
+    message: 'Field launched_at is invalid format',
+  })
+  launched_at?: Date;
 }
